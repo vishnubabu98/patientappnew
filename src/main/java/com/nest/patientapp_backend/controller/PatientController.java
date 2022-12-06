62120fce2e5b5df84ac47046cbdc812bb91a473b
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-06 23:48:00 +0530

    crossorigin

--- src/main/java/com/nest/patientapp_backend/controller/PatientController.java
+++ src/main/java/com/nest/patientapp_backend/controller/PatientController.java
@@ -3,10 +3,7 @@ package com.nest.patientapp_backend.controller;
 import com.nest.patientapp_backend.dao.PatientDao;
 import com.nest.patientapp_backend.model.Patients;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,6 +11,7 @@ import java.util.List;
 public class PatientController {
     @Autowired
     private PatientDao dao;
+    @CrossOrigin(origins = "*")
 
     @GetMapping("/view")
     public List<Patients> viewPage(){
