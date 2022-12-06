e414533d6566c06612be01d2b8c484a23b3fab8a
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-06 21:54:48 +0530

    initial

---
+++ src/main/java/com/nest/patientapp_backend/PatientappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.patientapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientappBackendApplication.class, args);
+	}
+
+}
